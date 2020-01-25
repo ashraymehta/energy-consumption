24baa7156a499f61b63d25d9fd17ec71eca2ed42
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-01-25 17:57:07 +0530

    Pulled equals and hashCode implementations into BaseModel.

---
+++ src/main/java/com/zenhomes/energyconsumption/models/BaseModel.java
@@ -0,0 +1,16 @@
+package com.zenhomes.energyconsumption.models;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class BaseModel {
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
