f11aed284f5cdb98b63c945affe7f769964dfc34
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-01-25 19:53:17 +0530

    Changed Jackson naming strategy to snake_case.
    * Added end-to-end test for counter_consumption endpoint.

--- src/main/java/com/zenhomes/energyconsumption/configuration/JacksonConfiguration.java
+++ src/main/java/com/zenhomes/energyconsumption/configuration/JacksonConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ public class JacksonConfiguration {
     public void configureJackson(ObjectMapper objectMapper) {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
     }
 
 }
