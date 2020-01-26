ce5b633aaccf6818714577b7c4b07851da595a32
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-01-26 13:29:05 +0530

    Logging in case of missing counter information in CounterGateway.

--- src/main/java/com/zenhomes/energyconsumption/gateways/CounterGateway.java
+++ src/main/java/com/zenhomes/energyconsumption/gateways/CounterGateway.java
@@ -2,16 +2,21 @@ package com.zenhomes.energyconsumption.gateways;
 
 import com.zenhomes.energyconsumption.configuration.CounterServiceConfiguration;
 import com.zenhomes.energyconsumption.models.dto.Counter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import static java.text.MessageFormat.format;
 
 @Component
 public class CounterGateway {
 
     private final RestTemplate restTemplate;
     private final CounterServiceConfiguration counterServiceConfiguration;
+    private final Logger logger = LoggerFactory.getLogger(CounterGateway.class);
 
     public CounterGateway(RestTemplateBuilder restTemplateBuilder,
                           CounterServiceConfiguration counterServiceConfiguration) {
@@ -20,11 +25,16 @@ public class CounterGateway {
     }
 
     public Counter getCounter(String counterId) {
-        final var uri = UriComponentsBuilder.fromHttpUrl(counterServiceConfiguration.getBaseUrl())
-                .path(GetCounterApi.PATH)
-                .queryParam(GetCounterApi.QUERY_PARAM_COUNTER_ID, counterId)
-                .build().toUri();
-        return restTemplate.getForObject(uri, Counter.class);
+        try {
+            final var uri = UriComponentsBuilder.fromHttpUrl(counterServiceConfiguration.getBaseUrl())
+                    .path(GetCounterApi.PATH)
+                    .queryParam(GetCounterApi.QUERY_PARAM_COUNTER_ID, counterId)
+                    .build().toUri();
+            return restTemplate.getForObject(uri, Counter.class);
+        } catch (Exception ex) {
+            logger.error(format("Could not find counter information for id [{0}]", counterId));
+            throw ex;
+        }
     }
 
     interface GetCounterApi {
